5de8b463ddeda1fd9bcb6fa0d98b82a3231cc963
ThomoShilubane <thomoraul@gmail.com>
2023-02-16 19:42:40 +0000

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 
 /**
  * main - A program that prints a line using the printf fuction
- * Return \: 0 (Success)
+ * Return: 0 Always
  */
 
 int main(void)
 
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n")
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return(0);
 }
