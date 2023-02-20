0fac81a47b723c44eb97fcedfcb070eef8b35d35
ThomoShilubane <thomoraul@gmail.com>
2023-02-20 18:55:31 +0000

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,7 +17,7 @@ int main(void)
 		{
 			for (c = '1'; c <= '9'; c++)
 			{
-				if ((a != b)&&(b != c)&&(a < b)&&(b < c))
+				if ((a != b) && (b != c) && (a < b) && (b < c))
 				{
 					putchar(a);
 					putchar(b);
