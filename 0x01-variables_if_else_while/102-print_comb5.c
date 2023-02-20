d64e19d0d8efe1a25de0c40c082efb827ec1717b
ThomoShilubane <thomoraul@gmail.com>
2023-02-20 20:19:16 +0000

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,7 +7,39 @@
 
 int main(void)
 {
-
+	int a;
+	int b;
+	int c;
+	int d;
+	
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = '0'; c <= '9'; c++)
+			{
+				for (d = '0'; d <= '9'; d++)
+				{
+					if (!(a == c && b == d))
+					{
+						if ((a <= c && b <= d))
+						{
+							putchar(a);
+							putchar(b);
+							putchar(' ');
+							putchar(c);
+							putchar(d);
+							if (!(a == '9' && b == '8'))
+							{
+								putchar(',');
+								putchar(' ');
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 
 	return (0);
 }
