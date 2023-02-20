7702812a33dc6e6e12ed842fbda5fa7ad51fdcf8
ThomoShilubane <thomoraul@gmail.com>
2023-02-20 18:46:08 +0000

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,5 +7,27 @@
 
 int main(void)
 {
+	int a; /*Hundreds*/
+	int b; /*Tens*/
+	int c; /*Units*/
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = '1'; c <= '9'; c++)
+			{
+				if ((a != b)&&(b != c)&&(a < b)&&(b < c))
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
 	return (0);
 }
