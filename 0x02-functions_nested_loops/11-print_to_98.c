f970727b9e56219c5df2e36583109dedd04e024f
ThomoShilubane <thomoraul@gmail.com>
2023-02-22 17:16:48 -0800

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,11 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * print_to_98 -  prints all natural numbers from n to 98.
- * @n - function parameter
+ * @n: function parameter
  */
+
 void print_to_98(int n)
 {
 	int a;
