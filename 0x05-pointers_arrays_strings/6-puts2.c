2bbadabbd2e7401ddf747d174b2cb9a146e96c47
ThomoShilubane <thomoraul@gmail.com>
2023-02-28 16:29:29 +0000

    6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,16 +8,17 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int t = 0;
+	int o;
 
-	while (str[i] != '\n')
+	while (str[t] != '\0')
 	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
+		t++;
 
-		i++;
+	}
+	for (o = 0; o < t; o += 2)
+	{
+		_putchar(str[o]);
 	}
 	_putchar('\n');
 }
