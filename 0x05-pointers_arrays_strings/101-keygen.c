948b2c66415e3354333a2e10bb35dfcf17e496aa
ThomoShilubane <thomoraul@gmail.com>
2023-02-28 18:16:10 +0000

    101-keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 
@@ -10,63 +10,25 @@
 
 int main(void)
 {
-	char passwd[84];
-	int index = 0, sum = 0, diff_half1, diff_half2;
-
-	srand(time(0));
-
-	while (sum < 2772)
-	{
-		passwd[index] = 33 + rand() % 94;
-		sum += passwd[index++];
-	}
-
-	passwd[index] = '\0';
-	if (sum != 2772)
-	{
-		diff_half1 = (sum - 2772) / 2;
-		diff_half2 = (sum - 2772) / 2;
-
-		if ((sum - 2772) % 2 != 0)
-			diff_half1++;
-		for (index = 0; passwd[index]; index++)
-		{
-			if (passwd[index] >= (33 + diff_half1))
-			{
-				passwd[index] -= diff_half1;
-				break;
-			}
-		}
-		for (index =  0; passwd[index]; index++)
-		{
-			if (passwd[index] >= (33 + diff_half2))
-			{
-				passwd[index] -= diff_half2;
-				break;
-			}
-		}
-	}
-	printf("%s", passwd);
-	return (0);
-}
-/**int i, sum, n;
+	int i, sum, n;
 	int pass[100];
 
 	sum = 0;
+
 	srand(time(NULL));
 
 	for (i = 0; i < 100; i++)
 	{
 		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
-		_putchar(pass[i] + '0');
+		putchar(pass[i] + '0');
 		if ((2772 - sum) - '0' < 78)
 		{
 			n = 2772 - sum - '0';
 			sum += n;
-			_putchar(n + '0');
+			putchar(n + '0');
 			break;
 		}
 	}
 	return (0);
-}**/
+}
