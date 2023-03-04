5c360ee5a9d0f600f3fd0c794984d3ebf4538f5a
ThomoShilubane <thomoraul@gmail.com>
2023-03-04 04:32:54 +0200

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,9 +1,7 @@
-#include "main.h"
 /**
  ** rev_string - reverse array
  ** @n: integer params
  ** Return: 0
- **/
 void rev_string(char *n)
 {
 	int i = 0;
@@ -23,14 +21,13 @@ void rev_string(char *n)
 		*(n + i) = temp;
 	}
 }
-/**
+/
  ** infinite_add - add 2 numbers together
  ** @n1: text representation of 1st number to add
  ** @n2: text representation of 2nd number to add
  ** @r: pointer to buffer
  ** @size_r: buffer size
  ** Return: pointer to calling function
- **/
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int overflow = 0, i = 0, j = 0, digits = 0;
@@ -77,4 +74,108 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	*(r + digits) = '\0';
 	rev_string(r);
 	return (r);
+}
+**/
+ #include "main.h"
+
+
+
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+
+
+/**
+
+ * add_strings - Adds the numbers stored in two strings.
+
+ * @n1: The string containing the first number to be added.
+
+ * @n2: The string containing the second number to be added.
+
+ * @r: The buffer to store the result.
+
+ * @r_index: The current index of the buffer.
+
+ *
+
+ * Return: If r can store the sum - a pointer to the result.
+
+ *         If r cannot store the sum - 0.
+
+ */
+
+char *add_strings(char *n1, char *n2, char *r, int r_index)
+
+{
+
+        int num, tens = 0;
+
+
+
+        for (; *n1 && *n2; n1--, n2--, r_index--)
+
+        {
+
+                num = (*n1 - '0') + (*n2 - '0');
+
+                num += tens;
+
+                *(r + r_index) = (num % 10) + '0';
+
+                tens = num / 10;
+
+        }
+
+
+
+        for (; *n1; n1--, r_index--)
+
+        {
+
+                num = (*n1 - '0') + tens;
+
+                *(r + r_index) = (num % 10) + '0';
+
+                tens = num / 10;
+
+        }
+
+
+
+        for (; *n2; n2--, r_index--)
+
+        {
+
+                num = (*n2 - '0') + tens;
+
+                *(r + r_index) = (num % 10) + '0';
+
+                tens = num / 10;
+
+        }
+
+
+
+        if (tens && r_index >= 0)
+
+        {
+
+                *(r + r_index) = (tens % 10) + '0';
+
+                return (r + r_index);
+
+        }
+
+
+
+        else if (tens && r_index < 0)
+
+                return (0);
+
+
+
+        return (r + r_index + 1);
+
 }
