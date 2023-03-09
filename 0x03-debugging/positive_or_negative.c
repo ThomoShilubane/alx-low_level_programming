2c421f4a15c0c0f815028d3c1db397839ddd93be
ThomoShilubane <thomoraul@gmail.com>
2023-03-09 17:00:25 +0000

    0x08. C - Recursion

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Function that runs a random number
+ * and prints wether it's negative, zero or positive
+ * more headers goes there
+ * betty style doc for function main goes there
+ * Return: 0 Always
+ */
+
+int main(void) /*Function that runs a random number*/
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0); /*Always*/
+}
